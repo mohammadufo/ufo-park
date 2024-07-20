fe696fe2f0eae71b967fb30303f7c1bba4ab02aa
Macbook <shadow.m.s.313@gmail.com>
2024-07-20 23:07:44 +0330

    confgure apollo client

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,27 @@
+'use client'
+
 import { add } from '@ufopark/sample-lib'
+import { useQuery } from '@apollo/client'
+import { CompaniesDocument } from '@ufopark/network/src/gql/generated'
 
 export default function Home() {
+  const { data, loading } = useQuery(CompaniesDocument)
+
+  console.log('data --->', data)
+
   return (
     <main>
       I love Alaa 💕
       <span>{add(2, 6)}</span>
+      <div>
+        <span>compony list</span>
+        {data?.companies.map((company) => (
+          <div className="p-4 bg-gray-100 rounded" key={company.id}>
+            <div>{company.displayName}</div>
+            <div>{company.description}</div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
---
+++ libs/network/src/config/apollo.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider as Provider,
+} from '@apollo/client'
+import { ReactNode } from 'react'
+
+export interface IApolloProviderProps {
+  children: ReactNode
+}
+
+export const ApolloProvider = ({ children }: IApolloProviderProps) => {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_API_URL + '/graphql',
+  })
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  })
+
+  // @ts-ignore
+  return <Provider client={apolloClient}>{children}</Provider>
+}
