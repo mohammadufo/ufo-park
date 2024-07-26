4de64306bc88391bf9bb814d88f97b3e9ab68662
Macbook <shadow.m.s.313@gmail.com>
2024-07-26 18:03:44 +0330

    implement map functionality part 2

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,5 +1,12 @@
+'use client'
+
 import { SearchPage } from '@ufopark/ui/src/components/templates/SearchPage'
+import { FormProviderSearchGarage } from '@ufopark/forms/src/searchGarages'
 
 export default function Page() {
-  return <SearchPage />
+  return (
+    <FormProviderSearchGarage>
+      <SearchPage />
+    </FormProviderSearchGarage>
+  )
 }
