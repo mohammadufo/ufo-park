c7dd3892c5eb575e09e56589ac3b51c7b4faf498
Macbook <shadow.m.s.313@gmail.com>
2024-07-29 21:15:31 +0330

    api - implemnt create garage and slotCounts

--- apps/api/src/models/garages/graphql/garages.resolver.ts
+++ apps/api/src/models/garages/graphql/garages.resolver.ts
@@ -7,7 +7,7 @@ import {
   Parent,
 } from '@nestjs/graphql'
 import { GaragesService } from './garages.service'
-import { Garage } from './entity/garage.entity'
+import { Garage, SlotTypeCount } from './entity/garage.entity'
 import { FindManyGarageArgs, FindUniqueGarageArgs } from './dtos/find.args'
 import { CreateGarageInput } from './dtos/create-garage.input'
 import { UpdateGarageInput } from './dtos/update-garage.input'
@@ -15,18 +15,22 @@ import { checkRowLevelPermission } from 'src/common/auth/util'
 import { GetUserType } from 'src/common/types'
 import { AllowAuthenticated, GetUser } from 'src/common/auth/auth.decorator'
 import { PrismaService } from 'src/common/prisma/prisma.service'
-import { Verification } from 'src/models/verifications/graphql/entity/verification.entity'
-import { Company } from 'src/models/companies/graphql/entity/company.entity'
-import { Address } from 'src/models/addresses/graphql/entity/address.entity'
 import { Slot } from 'src/models/slots/graphql/entity/slot.entity'
+import { Address } from 'src/models/addresses/graphql/entity/address.entity'
+import { Company } from 'src/models/companies/graphql/entity/company.entity'
+import { Verification } from 'src/models/verifications/graphql/entity/verification.entity'
 import {
   DateFilterInput,
   GarageFilter,
   MinimalSlotGroupBy,
 } from './dtos/search-filter.input'
-import { LocationFilterInput } from 'src/common/dtos/common.input'
+import {
+  AggregateCountOutput,
+  LocationFilterInput,
+} from 'src/common/dtos/common.input'
 import { SlotWhereInput } from 'src/models/slots/graphql/dtos/where.args'
 import { BadRequestException } from '@nestjs/common'
+import { GarageWhereInput } from './dtos/where.args'
 
 @Resolver(() => Garage)
 export class GaragesResolver {
@@ -37,8 +41,20 @@ export class GaragesResolver {
 
   @AllowAuthenticated('manager')
   @Mutation(() => Garage)
-  createGarage(@Args('createGarageInput') args: CreateGarageInput) {
-    return this.garagesService.create(args)
+  async createGarage(
+    @Args('createGarageInput') args: CreateGarageInput,
+    @GetUser() user: GetUserType,
+  ) {
+    const company = await this.prisma.company.findFirst({
+      where: { Managers: { some: { uid: user.uid } } },
+    })
+    if (!company?.id) {
+      throw new BadRequestException(
+        'No company associated with the manager id.',
+      )
+    }
+
+    return this.garagesService.create({ ...args, companyId: company.id })
   }
 
   @Query(() => [Garage], { name: 'garages' })
@@ -114,6 +130,24 @@ export class GaragesResolver {
         },
       },
     })
+  }
+
+  @ResolveField(() => [SlotTypeCount])
+  async slotCounts(@Parent() garage: Garage) {
+    const slotCounts = await this.prisma.slot.groupBy({
+      by: ['type'],
+      where: {
+        garageId: garage.id,
+      },
+      _count: {
+        type: true,
+      },
+    })
+
+    return slotCounts.map(({ type, _count }) => ({
+      type,
+      count: _count.type,
+    }))
   }
 
   @ResolveField(() => [MinimalSlotGroupBy], {
@@ -213,5 +247,19 @@ export class GaragesResolver {
   @ResolveField(() => [Slot])
   slots(@Parent() garage: Garage) {
     return this.prisma.slot.findMany({ where: { garageId: garage.id } })
+  }
+
+  @Query(() => AggregateCountOutput, {
+    name: 'garagesCount',
+  })
+  async garagesCount(
+    @Args('where', { nullable: true })
+    where: GarageWhereInput,
+  ) {
+    const garages = await this.prisma.garage.aggregate({
+      _count: { _all: true },
+      where,
+    })
+    return { count: garages._count._all }
   }
 }
