8c552af4e8662b1d601da6b6d82f81ac0e8a1d63
Macbook <shadow.m.s.313@gmail.com>
2024-08-07 22:52:10 +0330

    fix 3d issue

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
-
 import { CarScene } from '@ufopark/3d/src/scenes/CarScene'
+import { IconSearch } from '@tabler/icons-react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,8 +9,18 @@ export default function Home() {
       <div className="absolute top-16 bottom-0 left-0 right-0">
         <CarScene />
       </div>
+      <div className="flex flex-col items-start space-y-2 font-black text-8xl">
+        <div className="z-10 inline-block px-3 bg-primary mt-2">Need</div>{' '}
+        <div className="z-10 inline-block w-full max-w-md px-3 bg-primary ">
+          parking?
+        </div>
+        <Link
+          href="/search"
+          className="z-10 flex items-center gap-2 px-3 py-2 text-xl font-medium text-black underline underline-offset-4 bg-primary"
+        >
+          <IconSearch /> Search now
+        </Link>
+      </div>
     </main>
-
-    //* I love Alaa 💕
   )
 }
